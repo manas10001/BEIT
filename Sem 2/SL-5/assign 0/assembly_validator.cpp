10c02ba27e5b805e0c189be9b927daf5603ff326
MANAS <manas@manas-Inspiron-5567>
2020-01-01 00:06:42 +0530

    updated

--- Sem 2/SL-5/assign 0/assembly_validator.cpp
+++ Sem 2/SL-5/assign 0/assembly_validator.cpp
@@ -12,7 +12,7 @@ binary move
 #include<fstream>
 using namespace std;
 int main(){
-	string ar[]={"START","END","MOV","READ","WRITE"};
+	string ar[]={"START","END","MOV","READ","WRITE","ADD","DIV"};
 	
 	fstream fp;
 	fp.open("asm.txt");
@@ -27,15 +27,53 @@ int main(){
 		//on space or newline check the word	
 		if(c == ' ' || c == '\n'){
 			//check the word only if read is 0
-			for(int i = 0 ; i < 5 && read!=1 ; i++){
+			for(int i = 0 ; i < 7 && read!=1 ; i++){
 				if(strcmp(word.c_str(),ar[i].c_str())==0)
 				{
 					cout<<word<<endl;
 					read = 1;	//read will be used as a flag to indicate that the name of operation is read and no further reading is necessary for that line
 				}
+				if(read == 1){
+					
+						if( strcmp(word.c_str(),"START")==0 ){
+							//when start comes the next char should be newline char else its error
+							if(c == '\n')
+								break;
+							else{
+								cout<<"\n\tERROR ON LINE "<<lc<<" INVALID OPERATION THERE SHOUDNT BE ANYTHOING AFTER START";
+								return -1;
+							}
+						}
+						else if( strcmp(word.c_str(),"END") == 0 ){
+							//there shoud be nothing after end
+							if(fp.get(c)){
+								cout<<"\n\tERROR ON LINE "<<lc<<" INVALID OPERATION THERE SHOULD BE NOTHING AFTER END";
+									return -1;
+								}
+						}
+						else if( strcmp(word.c_str(),"READ") == 0 ){
+							//READ WRITE DIV TAKE ONLY ONE OPERATOR
+							if(c == '\n')
+								break;
+								
+							while(fp.get(c)){
+								if(c == '\n')
+									break;
+								if(c == ',' || c == ' '){
+									cout<<"\n\tERROR ON LINE "<<lc<<" ONLY ONE OPERATOR ALLOWED";
+									return -1;
+								}
+							}								
+						}
+								
+					
+				}
 			}
-			if(read == 0)
-				cout<<"ERROR ON LINE "<<lc<<" INVALID OPERATION "<<word;
+			if(read == 0){
+				cout<<"\n\tERROR ON LINE "<<lc<<" INVALID OPERATION "<<word;
+				return -1;
+			}
+			read = 1;	//read will be used as a flag to indicate that the name of operation is read and no further reading is necessary for that line
 				
 		word="";
 		if(c == '\n'){
