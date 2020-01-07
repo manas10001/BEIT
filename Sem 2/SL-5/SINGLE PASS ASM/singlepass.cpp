5144d9da187928d0a3b5f6210253e923c77ad99e
Manas Patil <34480896+manas10001@users.noreply.github.com>
2020-01-07 12:07:41 +0530

    Updated Single Pass Assembler

--- Sem 2/SL-5/SINGLE PASS ASM/singlepass.cpp
+++ Sem 2/SL-5/SINGLE PASS ASM/singlepass.cpp
@@ -1,22 +1,21 @@
 /*
 	SINGLE PASS ASSEMBLER
 
-	START	 200
-	MOVER	 AREG,	A
-L 	MOVEM	 BREG,	='2'
-	ADD	 BREG,	='2'
-	ADD	 CREG,	='3'
-	ORIGIN	 L+20
-	LTORG
-	MOVER	 AREG,	C
-C	EQU	 L+15
-	ADD	 AREG,	='2'
-	ADD	 BREG,	='5'
-A	DS	 5
-	END
+START 100	
+MOVER AREG,A		100
+L MOVEM	BREG,='2'	101	104
+ADD BREG,='2'		102
+ADD CREG,='3'		103	105
+ORIGIN L+20		
+LTORG			
+MOVER AREG,C		106
+C EQU L+15		
+ADD AREG,='2'		107	110
+ADD BREG,='5'		108	111
+A DS 5			109
+END
 
 */
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -34,7 +33,6 @@ string reg[3] = {"AREG", "BREG", "CREG"};																 //start from 1
 
 //SYMBOL TABLE
 int it_sytb = 0;
-//string sytb_sym[100];//int sytb_addr[100];
 struct symbtb
 {
 	string sym;
@@ -43,9 +41,10 @@ struct symbtb
 
 symbtb sytb[100];
 
+
+
 //LITERAL TABLE
 int it_lit = 0;
-//int lit_no[100];//string lit_lit[100];//int lit_addr[100];
 
 struct littb
 {
@@ -64,6 +63,8 @@ string litpt[100];
 int it_pool = 0;
 int pt[100];
 
+
+
 //ARRAY TO STORE LC PER LINE
 int arlc[100];
 
@@ -79,16 +80,21 @@ int curline = 0;
 //FLAG TO ENCOUNTER ,
 bool commaFlag = false;
 
-//FUNCTION DECLARATIONS			    //returns
-int is_keyword(string word);		//-1 for fail else code
+//FUNCTION DECLARATIONS			        //returns
+int is_keyword(string word);			//-1 for fail else code
 int is_mne(string word);			//-1 for fail else code
 int is_ad(string word);				//-1 for fail else code
 int is_dl(string word);				//-1 for fail else code
 int is_reg(string word);			//-1 for fail else code
-int in_sytb(string word, int addr); //returns -1 for fail else 1
-int is_constant(string word);		//-1 fro fail else integer value
-void printsytb();					//print syboltb
-void print_litpl();					//print literal pool
+int in_sytb(string word, int addr); 		//returns -1 for fail else 1
+int in_litpt(string word);			//returns -1 for fail else 1
+int is_constant(string word);			//-1 fro fail else integer value
+void print_sytb();				//print syboltb
+void print_litpl();				//print literal pool
+void print_poolt();				//print pool tabel
+void print_littb();				//print literal tabel	
+int process_lit(int addr);			//process literals after end or ltorg returns next lc
+
 
 int main()
 {
@@ -108,12 +114,19 @@ int main()
 		//possible breakpoints between words
 		if (c == ' ' || c == '\n' || c == '\t' || c == ',')
 		{
+			//need for , seperator processing
+			if (c == ',')
+			{
+				commaFlag = true;
+				//cout << "true\t";
+			}
+			//cout<<"\t\tSeperator: "<<c;
 			//cout<<word<<c;
 			//if we read start we will check the next word for being constant
 			//this block will execute only at start of code
 			if (chkconst == true)
 			{
-				cout << "checking const!\t";
+				//cout << "checking const!\t";
 				cnst = is_constant(word);
 				if (cnst == -1)
 				{
@@ -124,7 +137,7 @@ int main()
 				else
 				{
 					//assign const to lc
-					cout << "const: " << cnst;
+					//cout << "const: " << cnst<<endl;
 					lc = cnst;
 				}
 				chkconst = false;
@@ -156,6 +169,7 @@ int main()
 						return -1;
 					}
 				}
+				//end check
 				//check for end there should be nothing after end and literals are to be processed and lc to be manipulated
 				else if (strcmp(word.c_str(), ad[1].c_str()) == 0)
 				{
@@ -170,16 +184,30 @@ int main()
 					}
 					//if we complete this loop then everything is fine
 					//process literals and terminate
+					lc = process_lit(lc);
 					break;
 				}
 				else
 				{
-					//can be any keyword
-					lc++;
+					//shoudnt be reg
+					if(is_reg(word) == -1){
+						//cout<<"\tinc lc for: "<<word<<endl;
+						//can be any keyword
+						//origin restart lc from here
+						if( strcmp(word.c_str(),"ORIGIN") == 0 ){}
+						//ltorg	process literals
+						else if( strcmp(word.c_str(),"LTORG") == 0 )
+							lc = process_lit(lc);
+						//equ change lc for tht instruction only
+						else if( strcmp(word.c_str(),"EQU") == 0 ){}
+						//for other inst inc lc by 1
+						else
+							lc++;
+					}
 				}
 				line = 0; //reset the line flag afer reading keyword
 			}
-			else
+			else if(is_keyword(word) == -1)
 			{ //if the word is not a keyword then...
 				//can be label or literal or constant
 				//if line has started and its the first word which is not a keyword then consider it as a label and keep the line flag 1;
@@ -191,42 +219,39 @@ int main()
 					{
 						sytb[it_sytb].sym = word;
 						sytb[it_sytb].addr = lc;
-						cout << "symb: " << word << endl;
+						//cout << "symb: " << word << endl;
 						it_sytb++;
+						word = " ";
 					}
-				}
-				//after , next word will be a symb or literal so we set a flag anticipating the situation
-				if (c == ',')
-				{
-					commaFlag = true;
-					cout << "true\t";
-				}
-				if (commaFlag)
-				{
-					//literal starts with =
-					if (word[0] == '=')
+				}else{
+					
+					//after , next word will be a symb or literal so we set a flag anticipating the situation
+					
+					if (commaFlag)
 					{
-						//store in literal pool
-						litpt[it_litpool++] = word;
-					}
-					else
-					{
-						//it must be a symbol or const
-						if (is_constant(word) == -1)
+						//cout<<"after comma : "<<word;
+						//literal starts with =
+						if (word[0] == '=')
 						{
-							cout << "not const " << word;
-							//then its a symbol we have to check whether its already in the table
+							//store in literal pool
+							if(in_litpt(word)==-1)
+								litpt[it_litpool++] = word;
+						}
+						else
+						{
+							
 							if (in_sytb(word, 0) == -1)
 							{
 								//and if it is not add it in table without address
 								sytb[it_sytb].sym = word;
 								sytb[it_sytb].addr = 0;
-								cout << "symb nad: " << word << endl;
+								//cout << "symb nad: " << word << endl;
 								it_sytb++;
 							}
-						} //else its a constant
+						
+						}
+						commaFlag = false;
 					}
-					commaFlag = false;
 				}
 			}
 			if (c == '\n')
@@ -235,7 +260,7 @@ int main()
 				curline++;
 			}
 			word = "";
-		}
+		}//end for
 
 		//skip dividers
 		if (c == ' ' || c == '\n' || c == '\t' || c == ',')
@@ -248,9 +273,26 @@ int main()
 
 	fp.close();
 
-	printsytb();
+	print_sytb();
 	print_litpl();
+	print_poolt();
+	print_littb();
 	return 0;
+}
+
+
+int process_lit(int addr){
+		print_litpl();
+	//add new index of lit tabel to pool tabel
+	pt[it_pool++] = it_lit;
+	//put contents of literal pool to literal tabel and give add
+	for(int it = 0;it < it_litpool;it++){
+		lit[it_lit].no = it_lit;
+		lit[it_lit].literal = litpt[it];
+		lit[it_lit++].addr = addr++;				 
+	}
+	it_litpool = 0;
+	return addr;
 }
 
 //check if a symbol is already in the table if it is and has no address then assign it address
@@ -258,14 +300,14 @@ int in_sytb(string word, int addr)
 {
 	for (int it = 0; it < it_sytb; it++)
 	{
-		cout << "check " << word;
+		//cout << "check " << word<<endl;
 		if (strcmp(sytb[it].sym.c_str(), word.c_str()) == 0)
 		{
 			//set address if not already set
 			if (sytb[it].addr == 0 && addr != 0)
 			{
 				sytb[it].addr = addr;
-				cout << "Set ad to " << sytb[it].sym;
+				//cout << "Set ad to " << sytb[it].sym;
 			}
 			return 1;
 		}
@@ -273,8 +315,19 @@ int in_sytb(string word, int addr)
 	return -1;
 }
 
+//check if a literal is already in literal pool 
+int in_litpt(string word)
+{
+	for (int it = 0; it < it_litpool; it++)
+	{
+		if (strcmp(litpt[it].c_str(), word.c_str()) == 0)
+			return 1;
+	}
+	return -1;
+}
+
 //prints symbol table
-void printsytb()
+void print_sytb()
 {
 	cout << "\n\tSymbol Table:\n";
 	for (int it = 0; it < it_sytb; it++)
@@ -283,16 +336,29 @@ void printsytb()
 
 void print_litpl()
 {
-	cout << "Literal pool:";
+	cout << "\n\tLiteral pool:\n";
 	for (int it = 0; it < it_litpool; it++)
 		cout << " lit: " << litpt[it] << endl;
+}
+
+void print_poolt(){
+	cout<<"\n\tPool Tabel:\n";
+	for (int it = 0; it < it_pool; it++)
+		cout << " index: " << pt[it] << endl;
+}
+void print_littb(){
+	cout<<"\n\tLiteral Tabel:\n";
+	cout<<"\nNo\tLITERAL\tADDRESS\n";
+	for (int it = 0; it < it_lit; it++)
+		cout << lit[it].no <<"\t"<< lit[it].literal <<"\t"<<lit[it].addr<< endl;
+
 }
 
 //returns -1 if word is not keyword else returns its code
 int is_keyword(string word)
 {
 	int ret = -1;
-
+	//cout<<"\nchecking "<<word<<endl;
 	ret = is_mne(word);
 	//	cout<<word<<" ret mne : "<<ret<<endl;
 	if (ret == -1)
@@ -390,7 +456,6 @@ int is_constant(string word)
 
 /*
 blk1
-
 else{
 if(const != 0){
 //we may come here if we try to read a const after start
