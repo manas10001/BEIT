db0f7505f8e14069dbd2c868fdcc9396a0129501
Manas Patil <manaspatil1999np@gmail.com>
2021-02-04 23:29:04 +0530

    updates

--- BE/CL9/AS1/RMI/StringHash.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.*;
-
-//Define interface with method that we will access remotely
-public interface StringHash extends Remote {
-    int getHash(String str) throws RemoteException;
-}
