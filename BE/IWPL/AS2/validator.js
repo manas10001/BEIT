b7c82bfc3fe21dc2e704f49dd25f22d5028e9a79
Manas Patil <manaspatil1999np@gmail.com>
2021-02-07 00:21:53 +0530

    JS validations and dummy data removal

--- BE/IWPL/AS2/validator.js
+++ BE/IWPL/AS2/validator.js
@@ -15,3 +15,23 @@ function liveValidateStr(arg){
     else
         document.getElementById(arg).style.border = "1px solid red";
 }
+
+//validates size 2nd parameter should be length of 1st parameter
+function lengthValidate(arg, length){
+    var dat = document.getElementById(arg).value;
+    
+    if(dat.length != length)
+        document.getElementById(arg).style.border = "1px solid red";
+    else
+        document.getElementById(arg).style.border = "1px solid green";
+}
+
+//validates email with regexp
+function validateEmail(arg){
+    var dat = document.getElementById(arg).value;
+		var reg = /^([A-Za-z0-9_\-\.])+@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+		if(reg.test(dat)==true)
+			document.getElementById(arg).style.border = "1px solid green";
+		else
+			document.getElementById(arg).style.border = "1px solid red";
+}
