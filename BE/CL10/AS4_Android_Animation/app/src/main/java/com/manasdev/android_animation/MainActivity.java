df68969888260166df7c1910b4db2f3726c98fc2
Manas Patil <manaspatil1999np@gmail.com>
2021-03-07 16:06:56 +0530

    Android animation

--- BE/CL10/AS4_Android_Animation/app/src/main/java/com/manasdev/android_animation/MainActivity.java
+++ BE/CL10/AS4_Android_Animation/app/src/main/java/com/manasdev/android_animation/MainActivity.java
@@ -1,7 +1,9 @@
 package com.manasdev.android_animation;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -10,41 +12,68 @@ import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
     Animation rotate;
     Button speed_1x, speed_2x, speed_3x;
+    SeekBar dsb;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.imageView);
-        speed_1x = findViewById(R.id.rotate);
-        speed_2x = findViewById(R.id.rotate2);
-        speed_3x = findViewById(R.id.rotate3);
+//        speed_1x = findViewById(R.id.rotate);
+//        speed_2x = findViewById(R.id.rotate2);
+//        speed_3x = findViewById(R.id.rotate3);
+        dsb = findViewById(R.id.dsb);
 
-        speed_1x.setOnClickListener(new View.OnClickListener() {
+//        speed_1x.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                rotate(3000);
+//            }
+//        });
+//
+//        speed_2x.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                rotate(2000);
+//            }
+//        });
+//
+//        speed_3x.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                rotate(1000);
+//            }
+//        });
+
+        //seekbar handling
+        dsb.setMin(100);
+        dsb.setMax(5000);
+
+        dsb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onClick(View v) {
-                rotate(3000);
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    rotate(progress);
+                }
             }
-        });
 
-        speed_2x.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                rotate(2000);
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
             }
-        });
 
-        speed_3x.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                rotate(1000);
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
 
