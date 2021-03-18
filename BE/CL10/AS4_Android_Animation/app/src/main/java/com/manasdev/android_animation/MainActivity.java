9e3689ab6795989ea6756e2bd3aac86441ee57d7
Manas Patil <manaspatil1999np@gmail.com>
2021-03-18 16:56:42 +0530

    minor updates

--- BE/CL10/AS4_Android_Animation/app/src/main/java/com/manasdev/android_animation/MainActivity.java
+++ BE/CL10/AS4_Android_Animation/app/src/main/java/com/manasdev/android_animation/MainActivity.java
@@ -1,24 +1,21 @@
-package com.manasdev.android_animation;
+ package com.manasdev.android_animation;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
     Animation rotate;
-    Button speed_1x, speed_2x, speed_3x;
     SeekBar dsb;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -28,52 +25,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.imageView);
-//        speed_1x = findViewById(R.id.rotate);
-//        speed_2x = findViewById(R.id.rotate2);
-//        speed_3x = findViewById(R.id.rotate3);
         dsb = findViewById(R.id.dsb);
 
-//        speed_1x.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                rotate(3000);
-//            }
-//        });
-//
-//        speed_2x.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                rotate(2000);
-//            }
-//        });
-//
-//        speed_3x.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                rotate(1000);
-//            }
-//        });
-
         //seekbar handling
-        dsb.setMin(100);
+        dsb.setMin(3000);
         dsb.setMax(5000);
 
         dsb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 if(fromUser){
-                    rotate(progress);
+                    int rt = 5500-progress;
+                    rotate(rt);
+                    //Toast.makeText(MainActivity.this, ""+rt,Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
 
